d820abd0d281235b6c01f71554fb435ceff9a0d7
Ayush kumar <154700321+StrikerEureka34@users.noreply.github.com>
2024-07-29 18:34:01 +0530

    Tower Defense files

---
+++ Assets/Scripts/FindHome.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FindHome : MonoBehaviour {
+
+    public Transform destination;
+    NavMeshAgent ai;
+
+    void Start() {
+
+        ai = GetComponent<NavMeshAgent>();
+        ai.SetDestination(destination.position);
+    }
+
+
+    void Update() {
+
+        if (ai.remainingDistance < 0.5f && ai.hasPath) {
+
+            LevelManager.RemoveEnemy();
+            ai.ResetPath();
+            Destroy(this.gameObject, 0.1f);
+        }
+    }
+}
